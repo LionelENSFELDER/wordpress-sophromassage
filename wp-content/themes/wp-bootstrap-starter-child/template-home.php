2e6613dfe786b317eb00eab01b7a480acc0ab7af
Lionel ENSFELDER <83010-28-05@exchange.ad.afpanet>
2019-09-05 11:30:37 +0200

    add last articles overlay section

--- wp-content/themes/wp-bootstrap-starter-child/template-home.php
+++ wp-content/themes/wp-bootstrap-starter-child/template-home.php
@@ -475,6 +475,56 @@ get_header();
 
 
 
+<!-- Articles style => OVERLAY-->
+<section class="section w-100 p-5">
+    <div class="container">
+        <h1 class="ml-3 font-weight-bold font-hard-grey">Les derniers articles (overlay)</h1>
+
+        <div class="d-flex align-items-stretch flex-wrap">
+            <?php
+                $args = array(
+                    'orderby' => 'post_date',
+                    'category' => 2,
+                    'posts_per_page' => 3
+                );
+                $posts_array = get_posts($args);
+                foreach($posts_array as $post){
+                    $post_Id = $post->ID;
+                    ?>
+                    <div class="col-4 mb-5" style="height:500px;">
+                        <div class="card border-0 shadow h-100">
+
+                            <img class="card-img h-100" src="<?php the_post_thumbnail_url();?>" title="" alt="">
+
+                            <div class="card-img-overlay d-flex flex-column justify-content-end">
+                                <div class="d-flex flex-column justify-content-center">
+                                    <h2 class="text-white"><?php echo $post->post_title; ?></h2>
+                                    <div class="">
+                                        <span class="text-white">Auteur: <?php echo $post->post_author; ?></span>
+                                        <br />
+                                        <span class="text-white">Publié: <?php echo $post->post_date; ?></span>
+                                        <br />
+                                        <a href="<?php echo get_permalink($post_Id) ?>" class="stretched-link"></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+            ?>
+
+            <div class="col-12 text-center">
+                <button type="button" class="btn btn-main-full">Plus d'articles</button>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+
+
+
 <!-- full-width section template -->
 <section class="section w-100 p-5">
     <!-- CONTAINER -->
