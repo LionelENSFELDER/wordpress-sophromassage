2a2376ec36f681d614a1946c4006182e80cf44de
Lionel <ensfelder.lionel@gmail.com>
2020-01-02 17:58:05 +0100

    Improve navbar color change behaviour fix #18

--- wp-content/themes/wp-bootstrap-starter-child/js/navbar.js
+++ wp-content/themes/wp-bootstrap-starter-child/js/navbar.js
@@ -1,10 +1,39 @@
-window.onscroll = () => {
-	let navbar = document.getElementById("masthead");
-	console.log(navbar);
 
-	if (window.scrollY <= 80) {
-		navbar.classList.remove("navbar-scroll");
-	} else if (window.scrollY > 80) {
+function menuColor(){
+	let screenWidth = window.screen.width;
+	let navbar = document.getElementById("masthead");
+
+	if(screenWidth <= 1199){
+
 		navbar.classList.add("navbar-scroll");
+		console.log("navbar <= 1199");
+
+	}else if (screenWidth >= 1200){
+
+		window.addEventListener('scroll', function() {
+
+			let screenWidth = window.screen.width;
+			let navbar = document.getElementById("masthead");
+			
+			if (screenWidth > 1199 && window.scrollY <= 80){
+
+				navbar.classList.remove("navbar-scroll");
+				console.log("window.scrollY <= 80");
+
+			}else if (screenWidth > 1199 && window.scrollY > 80){
+
+				navbar.classList.add("navbar-scroll");
+				console.log("window.scrollY > 80");
+
+			}
+		})
+	}
+}
+
+
+document.onreadystatechange = function () {
+	if(document.readyState === "complete"){
+		menuColor();
 	}
 };
+window.onresize = menuColor;
